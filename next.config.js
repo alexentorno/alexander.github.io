034bfa4f10f8402ab1462c153204b17a54638422
alpekh <alpekh@taltech.ee>
2025-09-29 23:03:02 +0300

    css

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+    // For GitHub Pages deployment
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+    assetPrefix = `/${repo}/`;
+    basePath = `/${repo}`;
+}
+
 const nextConfig = {
-    reactStrictMode: true,
     output: 'export',
     trailingSlash: true,
     images: {
         unoptimized: true
     },
-
+    reactStrictMode: true,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
